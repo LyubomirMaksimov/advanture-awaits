df41f41e31cc909be10ba3f877f4709626009b4d
lyubomirmaksimov <lyubomir.maksimov@gmail.com>
2022-07-08 22:40:02 +0300

    finished

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import "../../App.css";
 import HeroSection from "../HeroSection";
 import React from "react";
 import Cards from "../Cards";
+import Footer from "../Footer";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 };
---
+++ src/components/pages/Products.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../App.css";
+
+const Producsts = () => {
+  return <h1 className="products">PRODUCTS</h1>;
+};
+
+export default Producsts;
